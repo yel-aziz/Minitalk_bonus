166b886b6bc9475b36b6ea98021e21b94b50039f
Yassir El azizi <88348802+yel-aziz@users.noreply.github.com>
2022-01-13 18:41:09 +0100

    Add files via upload

---
+++ mini/minitalk_bonus/minitalk_bonus.h
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk_bonus.h                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yel-aziz <yel-aziz@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/11 14:48:48 by yel-aziz          #+#    #+#             */
+/*   Updated: 2022/01/13 15:20:48 by yel-aziz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_BONUS_H
+# define MINITALK_BONUS_H
+
+# include <unistd.h>
+# include <stdio.h>
+# include <stdio.h>
+# include <signal.h>
+# include <stdlib.h>
+# include <stdarg.h>
+
+int	ft_putchar(char c);
+int	ft_printhex( unsigned long n);
+int	ft_putnbr_base(unsigned long n);
+int	ft_putnbr(int n);
+int	ft_puthex(unsigned long n);
+int	ft_putstr(char *s);
+int	ft_putnbrunsi(unsigned int n);
+int	ft_printf(const char *fmt, ...);
+int	ft_puthexup(unsigned long n);
+int	ft_atoi(const char *str);
+
+#endif
